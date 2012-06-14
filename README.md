66cdfda830326f12545ffe9feda6d41385f3738b
Robin Clowers <robin.clowers@gmail.com>
2012-06-14 05:56:32 -0700

    created rails app

---
+++ README.md
@@ -0,0 +1,4 @@
+# Rails 3.2 overview
+
+This is a sample application to show off Rails 3.2.
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails32Overview::Application.config.secret_token = '18adf0431d3ea73f33a6380b9ef433ccb4133e57b33d1ede041371c9624e72fd22cef3d7976751d073e8a20c926d0f8fd666b6e05679afc4cb186261ea6fab19'
