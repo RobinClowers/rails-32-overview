506052358473516af089e6ae71620a079abd039d
Robin Clowers <robin.clowers@gmail.com>
2012-06-15 13:09:26 -0700

    add comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :title
+  has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails32Overview::Application.routes.draw do
   root :to => "home#index"
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
